e7850ef63709774a457c651c851210da5afec151
kridi32 <kridi32@outlook.com>
2022-04-20 17:32:16 +0600

    basic server setup done and it is workin g

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("this is the first line of this server");
+const  express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => { 
+    res.send("welcome to our server")
+})
+
+app.listen(port, () => {
+    console.log("server is working");
+})
