a4763286b6adc50a4859ba5558b401855555783c
kridi32 <kridi32@outlook.com>
2022-04-20 18:52:22 +0600

    cors added to share resources between two ports of local host

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const  express = require("express");
+const cors = require("cors");// this is cors aka cross origin resource sharing that allows us to share data between backend and frontend 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors());
 
 //creating some data 
 const user = {name:"Abdul basir", university: "Dhaka", department:"Management Informations Systems", course:"Bangladesh sturdies", batch:16, ischairman:false};
