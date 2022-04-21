43841f71c6f075ccde629788f0bed928175cb6ed
kridi32 <kridi32@outlook.com>
2022-04-21 11:40:44 +0600

    we can send data from frontend to backend using post method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from 'react'
+import "./App.css";
+import {  useState } from "react";
 
 function App() {
-  const [user, setUser]  = useState({});
-  useEffect( () => {
-    fetch('http://localhost:4000/')
-    .then(res => res.json())
-    .then(data => setUser(data))
-  },[])
+  const [name, setName] = useState("");
+  const [roll, setRoll] = useState("");
+  const [users, setUsers] = useState([{ id: 1, name: "Prodosh Mitter", roll: "565" }]);
+ 
+
+  const handleStudenInfoSubmit = (e) => {
+    e.preventDefault();
+    const student = { name: name, roll: roll };
+    console.log(student);
+    // post data to server
+    fetch("http://localhost:4000/testing", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(student),
+    })
+      .then((res) => res.json())
+      .then((data) =>  {
+        console.log(data);
+        const tempUsers = [...users, data];
+        setUsers(tempUsers);
+      });
+  };
   return (
     <div className="App">
-      <p>{user.name}</p>
+      
+      <hr />
+      <h1>Student information</h1>
+      <form action="" onSubmit={handleStudenInfoSubmit}>
+        <label htmlFor="name">
+          Name{" "}
+          <input
+            type="text"
+            name="name"
+            onBlur={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="roll">
+          Roll number
+          <input
+            type="text"
+            name="roll"
+            onBlur={(e) => setRoll(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit info</button>
+      </form>
+      <div className="">{
+           users.map(user => {
+             return (
+               <p>{user.name}</p>
+             )
+           })
+      }</div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const  express = require("express");
-const cors = require("cors");// this is cors aka cross origin resource sharing that allows us to share data between backend and frontend 
+const express = require("express");
+const cors = require("cors"); // this is cors aka cross origin resource sharing that allows us to share data between backend and frontend
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
+//for pursing data from body in post
+app.use(express.json());
 
-//creating some data 
-const user = {name:"Abdul basir", university: "Dhaka", department:"Management Informations Systems", course:"Bangladesh sturdies", batch:16, ischairman:false};
-app.get("/", (req, res) => { 
-    res.send(user)
-})
+//creating some data
+const users = [];
+// getting data from client side
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send(users);
+});
+// geting data from backend
+app.get("/testing", (req, res) => {
+  console.log(req);
+  res.send({ name: "testing" });
+  console.log("Successfully sent the data from client side");
+});
+
+// posting data from frontend
+app.post("/testing", (req, res) => {
+  let user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
+});
 
 app.listen(port, () => {
-    console.log("server is working");
-})
+  console.log("server is working");
+});
